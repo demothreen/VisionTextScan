c236175524e067bc1e15f7945e082479772b26e8
demothreen <demothreen@me.com>
2020-10-14 21:39:53 +0500

    Initial Commit

---
+++ VisionTextScan/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VisionTextScan
+//
+//  Created by demothreen on 14.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
